2708660a37bc0edf736a5897f71282bf43e6b6ac
adriano <lussoadriano@gmail.com>
2025-04-08 13:35:05 -0300

    ammend

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# CRUD_NetAngular
+# CRUD con Angular y .NET
+
+Este repositorio implementa un gestor de stock de ropa. El front-end fue realizado con Angular 19 y el back-end con ASP.NET 9. Se utilizó una base de datos postgre inicializada en el servicio cloud Tembo.
+
+El subproyecto de Angular se encuentra en directorio 'app'. Las librerías secundarias utilizada son (entre otras):
+
+*  Angular Material para ciertos componentes personalizados.
+* SweetAlert2 para un pop-up personalizado.
+
+El subproyecto de ASP.NET se encuentra en el directorio 'server'.
--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+043e42a887e34a936014f8bb9fd00f955b34f4fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a7bcf70d23ea55e15e9554b397a308db52abc76")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
